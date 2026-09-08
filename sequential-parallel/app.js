036da97b6a1f22d43ce5861a36f5402daba050eb
Mark Lhester Pile <pilemarklhester028@gmail.com>
2026-09-08 13:25:04 +0800

    fix: created the sequential logic

--- sequential-parallel/app.js
+++ sequential-parallel/app.js
@@ -1,25 +1,42 @@
-const getUserProfile = async () => {
-  try {
-    const fetchUserData = await fetch(
-      "https://jsonplaceholder.typicode.com/users/",
+//Sequential
+const commentLists = document.getElementById("comments__list");
+const getPostWithComments = async (postID) => {
+  const fetchPost = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${postID}`,
+  );
+
+  if (!fetchPost.ok) {
+    throw new Error(
+      "An error occurred while fetching the data. Try again later",
     );
-
-    if (fetchUserData.status === 404 || !fetchUserData.ok) {
-      throw new Error("There's a problem with the server. Try again later");
-    }
-
-    const userData = await fetchUserData.json();
-
-    console.log(userData);
-
-    userData.forEach((data) => {
-      if (data.id) {
-        console.log(data.address.street);
-      }
-    });
-  } catch (err) {
-    console.log("Error: ", err.message);
   }
+
+  const userData = await fetchPost.json();
+  const fetchUserPosts = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${userData.id}`,
+  );
+
+  if (!fetchUserPosts.ok) {
+    throw new Error("Comment cannot be fetch. Try later");
+  }
+
+  const userPost = await fetchUserPosts.json();
+  console.log(userPost);
+  
+  renderUserData(userPost);
 };
 
-console.log(getUserProfile());
+function renderUserData(userPost) {
+  commentLists.innerHTML += userPost.map((data) => {
+    return `<li class="comment-details">
+      <p>${data.name}</p>
+      <p>${data.email}</p>
+      <p>${data.body}</p>
+    </li>`;
+  }).join('');
+}
+
+//if block body {} => use return
+//if expression body () => no return => results in object literal
+
+getPostWithComments(1);
